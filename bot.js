c7b14e859ed8890f756a78958ff913746c357bff
telgar <2283474+telgar@users.noreply.github.com>
2018-04-16 22:29:51 +0100

    CI and CD test

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@ const _ = require('lodash');
 const Discord = require('discord.js');
 
 const config = require('./config.json');
+
 const client = new Discord.Client();
 const fs = require('fs');
 
--- commands/botinfo.js
+++ commands/botinfo.js
@@ -2,6 +2,8 @@ const { version } = require("discord.js");
 const moment = require("moment");
 require("moment-duration-format");
 
+const pjson = require('../package.json');
+
 const self = module.exports;
 
 exports.config = {    
@@ -19,13 +21,12 @@ exports.run = (client, message, args) => {
     const duration = moment.duration(client.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
     message.channel.send(
 `= Bot Info =
-
-• Mem Usage  :: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB
-• Uptime     :: ${duration}
-• Users      :: ${client.users.size.toLocaleString()}
-• Servers    :: ${client.guilds.size.toLocaleString()}
-• Channels   :: ${client.channels.size.toLocaleString()}
-• Discord.js :: v${version}
-• Node       :: ${process.version}`, {code: "asciidoc"});
-
+• Bot version :: ${pjson.version}
+• Mem Usage   :: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB
+• Uptime      :: ${duration}
+• Users       :: ${client.users.size.toLocaleString()}
+• Servers     :: ${client.guilds.size.toLocaleString()}
+• Channels    :: ${client.channels.size.toLocaleString()}
+• Discord.js  :: v${version}
+• Node        :: ${process.version}`, {code: "asciidoc"});
 };
